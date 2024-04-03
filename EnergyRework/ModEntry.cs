9987af0a24caf5c32f481c302c88ee9bf21b3446
Asim Seth <131764979+seferoni@users.noreply.github.com>
2024-04-03 08:08:12 +0630

    Initial commit

---
+++ EnergyRework/ModEntry.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace EnergyRework
+{
+	internal sealed class ModEntry : Mod
+	{
+		private readonly Dictionary<string, float> Parameters = new()
+		{
+			{"BaseEnergyLoss", 1f},
+			{"EnergyFloor", 0f},
+			{"MovingEnergyOffset", 2f}
+		};
+		public override void Entry(IModHelper helper)
+		{
+			helper.Events.GameLoop.TimeChanged += this.TimeChanged;
+		}
+
+		private void ReduceEnergy(float energyLoss)
+		{
+			float currentStamina = Game1.player.Stamina;
+
+			if (currentStamina <= this.Parameters["EnergyFloor"])
+			{
+				return;
+			}
+
+			Game1.player.Stamina = Math.Max(this.Parameters["EnergyFloor"], currentStamina - energyLoss);
+		}
+
+		private void TimeChanged(object? sender, TimeChangedEventArgs e)
+		{
+			if (!Context.IsWorldReady)
+			{
+				return;
+			}
+
+			if (!Context.IsPlayerFree)
+			{
+				return;
+			}
+
+			if (Game1.paused)
+			{
+				return;
+			}
+
+			float energyLoss = this.Parameters["BaseEnergyLoss"];
+
+			if (Game1.player.running && Game1.player.isMoving())
+			{
+				energyLoss += this.Parameters["MovingEnergyOffset"];
+			}
+
+			this.ReduceEnergy(energyLoss);
+		}
+	}
+}
